0c4511f7f0bfc6095eac449226bf1c07cf6b013e
OPTIMUSGYZ <optimus.gyz@gmail.com>
2022-07-06 20:57:04 -0400

    corrected some misplaced data

---
+++ data/rename.py
@@ -0,0 +1,11 @@
+import os
+
+people = [12, 26, 28, 36, 43, 47, 52, 56, 57, 58, 59, 60]
+n = 0
+for p in people:
+    for i in range(10):
+        for j in range(10):
+            file = "./audio/{}/{}_{}_{}.wav".format(i, i, p, j)
+            out = "./audio/{}/{}_{}.wav".format(i, n, j)
+            os.rename(file, out)
+    n += 1
