b7b360c6e7a03e6bb07fd4c5be5863f2fb6d932a
OPTIMUS_GYZ <optimus.gyz@gmail.com>
2022-07-22 13:24:17 -0400

    better model?

--- code/training.py
+++ code/training.py
@@ -7,9 +7,9 @@ use_metal = False
 #################
 
 
-batch_size = 400
-lr = 0.0005
-epoch = 15
+batch_size = 250
+lr = 0.00049
+epoch = 20
 if train_mode:
     start_training(batch_size, lr, epoch, use_cuda, use_metal)
 show_model_test_accuracy(batch_size, lr, epoch - 1)  # default load to last epoch
